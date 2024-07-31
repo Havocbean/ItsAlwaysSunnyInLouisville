f015101070d76d0efb7a57580f8a8ec4ddeec5d7
Havocbean <bassichavoc@gmail.com>
2024-07-30 23:20:18 -0700

    Initial commit. v.02

---
+++ ItsAlwaysSunnyInLouisville/Contents/mods/ItsAlwaysSunnyInLouisville/media/lua/server/items/ItsAlwaysSunnyInLouisvilleDistributions.lua
@@ -0,0 +1,167 @@
+require "Items/Distributions"
+require "Items/ItemPicker"
+require "Items/ProceduralDistributions"
+
+-- Individual Can
+table.insert(ProceduralDistributions["list"]["FridgeTrailerPark"].items, "ItsAlwaysSunnyInLouisville.WolfCola");
+table.insert(ProceduralDistributions["list"]["FridgeTrailerPark"].items, 2);
+table.insert(ProceduralDistributions["list"]["MotelFridge"].items, "ItsAlwaysSunnyInLouisville.WolfCola");
+table.insert(ProceduralDistributions["list"]["MotelFridge"].items, 1);
+table.insert(ProceduralDistributions["list"]["CrateSodaCans"].items, "ItsAlwaysSunnyInLouisville.WolfCola");
+table.insert(ProceduralDistributions["list"]["CrateSodaCans"].items, 1);
+table.insert(ProceduralDistributions["list"]["GigamartBottles"].items, "ItsAlwaysSunnyInLouisville.WolfCola");
+table.insert(ProceduralDistributions["list"]["GigamartBottles"].items, 115);
+table.insert(ProceduralDistributions["list"]["KitchenBottles"].items, "ItsAlwaysSunnyInLouisville.WolfCola");
+table.insert(ProceduralDistributions["list"]["KitchenBottles"].items, 1);
+table.insert(ProceduralDistributions["list"]["StoreShelfCombo"].items, "ItsAlwaysSunnyInLouisville.WolfCola");
+table.insert(ProceduralDistributions["list"]["StoreShelfCombo"].items, 1);
+table.insert(ProceduralDistributions["list"]["StoreShelfDrinks"].items, "ItsAlwaysSunnyInLouisville.WolfCola");
+table.insert(ProceduralDistributions["list"]["StoreShelfDrinks"].items, 1);
+table.insert(ProceduralDistributions["list"]["TheatreDrinks"].items, "ItsAlwaysSunnyInLouisville.WolfCola");
+table.insert(ProceduralDistributions["list"]["TheatreDrinks"].items, 1);
+table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, "ItsAlwaysSunnyInLouisville.WolfCola");
+table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, 1);
+table.insert(ProceduralDistributions["list"]["BandPracticeFridge"].items, "ItsAlwaysSunnyInLouisville.WolfCola");
+table.insert(ProceduralDistributions["list"]["BandPracticeFridge"].items, 1);
+table.insert(ProceduralDistributions["list"]["BarCounterMisc"].items, "ItsAlwaysSunnyInLouisville.WolfCola");
+table.insert(ProceduralDistributions["list"]["BarCounterMisc"].items, 1);
+table.insert(ProceduralDistributions["list"]["CafeteriaDrinks"].items, "ItsAlwaysSunnyInLouisville.WolfCola");
+table.insert(ProceduralDistributions["list"]["CafeteriaDrinks"].items, 1);
+table.insert(ProceduralDistributions["list"]["ClassroomDesk"].items, "ItsAlwaysSunnyInLouisville.WolfCola");
+table.insert(ProceduralDistributions["list"]["ClassroomDesk"].items, 1);
+table.insert(ProceduralDistributions["list"]["FridgeBreakRoom"].items, "ItsAlwaysSunnyInLouisville.WolfCola");
+table.insert(ProceduralDistributions["list"]["FridgeBreakRoom"].items, 1);
+table.insert(ProceduralDistributions["list"]["FridgeOffice"].items, "ItsAlwaysSunnyInLouisville.WolfCola");
+table.insert(ProceduralDistributions["list"]["FridgeOffice"].items, 1);
+table.insert(ProceduralDistributions["list"]["FridgeSoda"].items, "ItsAlwaysSunnyInLouisville.WolfCola");
+table.insert(ProceduralDistributions["list"]["FridgeSoda"].items, 2);
+
+
+
+table.insert(ProceduralDistributions["list"]["FridgeTrailerPark"].items, "ItsAlwaysSunnyInLouisville.Invigaron");
+table.insert(ProceduralDistributions["list"]["FridgeTrailerPark"].items, 2);
+table.insert(ProceduralDistributions["list"]["MotelFridge"].items, "ItsAlwaysSunnyInLouisville.Invigaron");
+table.insert(ProceduralDistributions["list"]["MotelFridge"].items, 1);
+table.insert(ProceduralDistributions["list"]["CrateSodaCans"].items, "ItsAlwaysSunnyInLouisville.Invigaron");
+table.insert(ProceduralDistributions["list"]["CrateSodaCans"].items, 1);
+table.insert(ProceduralDistributions["list"]["GigamartBottles"].items, "ItsAlwaysSunnyInLouisville.Invigaron");
+table.insert(ProceduralDistributions["list"]["GigamartBottles"].items, 115);
+table.insert(ProceduralDistributions["list"]["KitchenBottles"].items, "ItsAlwaysSunnyInLouisville.Invigaron");
+table.insert(ProceduralDistributions["list"]["KitchenBottles"].items, 1);
+table.insert(ProceduralDistributions["list"]["StoreShelfCombo"].items, "ItsAlwaysSunnyInLouisville.Invigaron");
+table.insert(ProceduralDistributions["list"]["StoreShelfCombo"].items, 1);
+table.insert(ProceduralDistributions["list"]["StoreShelfDrinks"].items, "ItsAlwaysSunnyInLouisville.Invigaron");
+table.insert(ProceduralDistributions["list"]["StoreShelfDrinks"].items, 1);
+table.insert(ProceduralDistributions["list"]["TheatreDrinks"].items, "ItsAlwaysSunnyInLouisville.Invigaron");
+table.insert(ProceduralDistributions["list"]["TheatreDrinks"].items, 1);
+table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, "ItsAlwaysSunnyInLouisville.Invigaron");
+table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, 1);
+table.insert(ProceduralDistributions["list"]["BandPracticeFridge"].items, "ItsAlwaysSunnyInLouisville.Invigaron");
+table.insert(ProceduralDistributions["list"]["BandPracticeFridge"].items, 1);
+table.insert(ProceduralDistributions["list"]["BarCounterMisc"].items, "ItsAlwaysSunnyInLouisville.Invigaron");
+table.insert(ProceduralDistributions["list"]["BarCounterMisc"].items, 1);
+table.insert(ProceduralDistributions["list"]["CafeteriaDrinks"].items, "ItsAlwaysSunnyInLouisville.Invigaron");
+table.insert(ProceduralDistributions["list"]["CafeteriaDrinks"].items, 1);
+table.insert(ProceduralDistributions["list"]["ClassroomDesk"].items, "ItsAlwaysSunnyInLouisville.Invigaron");
+table.insert(ProceduralDistributions["list"]["ClassroomDesk"].items, 1);
+table.insert(ProceduralDistributions["list"]["FridgeBreakRoom"].items, "ItsAlwaysSunnyInLouisville.Invigaron");
+table.insert(ProceduralDistributions["list"]["FridgeBreakRoom"].items, 1);
+table.insert(ProceduralDistributions["list"]["FridgeOffice"].items, "ItsAlwaysSunnyInLouisville.Invigaron");
+table.insert(ProceduralDistributions["list"]["FridgeOffice"].items, 1);
+table.insert(ProceduralDistributions["list"]["FridgeSoda"].items, "ItsAlwaysSunnyInLouisville.Invigaron");
+table.insert(ProceduralDistributions["list"]["FridgeSoda"].items, 2);
+
+
+table.insert(ProceduralDistributions["list"]["MotelFridge"].items, "ItsAlwaysSunnyInLouisville.FightMilk");
+table.insert(ProceduralDistributions["list"]["MotelFridge"].items, 1);
+table.insert(ProceduralDistributions["list"]["CrateSodaCans"].items, "ItsAlwaysSunnyInLouisville.FightMilk");
+table.insert(ProceduralDistributions["list"]["CrateSodaCans"].items, 1);
+table.insert(ProceduralDistributions["list"]["GigamartBottles"].items, "ItsAlwaysSunnyInLouisville.FightMilk");
+table.insert(ProceduralDistributions["list"]["GigamartBottles"].items, 115);
+table.insert(ProceduralDistributions["list"]["KitchenBottles"].items, "ItsAlwaysSunnyInLouisville.FightMilk");
+table.insert(ProceduralDistributions["list"]["KitchenBottles"].items, 1);
+table.insert(ProceduralDistributions["list"]["StoreShelfCombo"].items, "ItsAlwaysSunnyInLouisville.FightMilk");
+table.insert(ProceduralDistributions["list"]["StoreShelfCombo"].items, 1);
+table.insert(ProceduralDistributions["list"]["StoreShelfDrinks"].items, "ItsAlwaysSunnyInLouisville.FightMilk");
+table.insert(ProceduralDistributions["list"]["StoreShelfDrinks"].items, 1);
+table.insert(ProceduralDistributions["list"]["TheatreDrinks"].items, "ItsAlwaysSunnyInLouisville.FightMilk");
+table.insert(ProceduralDistributions["list"]["TheatreDrinks"].items, 1);
+table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, "ItsAlwaysSunnyInLouisville.FightMilk");
+table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, 1);
+table.insert(ProceduralDistributions["list"]["BandPracticeFridge"].items, "ItsAlwaysSunnyInLouisville.FightMilk");
+table.insert(ProceduralDistributions["list"]["BandPracticeFridge"].items, 1);
+table.insert(ProceduralDistributions["list"]["BarCounterMisc"].items, "ItsAlwaysSunnyInLouisville.FightMilk");
+table.insert(ProceduralDistributions["list"]["BarCounterMisc"].items, 1);
+table.insert(ProceduralDistributions["list"]["CafeteriaDrinks"].items, "ItsAlwaysSunnyInLouisville.FightMilk");
+table.insert(ProceduralDistributions["list"]["CafeteriaDrinks"].items, 1);
+table.insert(ProceduralDistributions["list"]["ClassroomDesk"].items, "ItsAlwaysSunnyInLouisville.FightMilk");
+table.insert(ProceduralDistributions["list"]["ClassroomDesk"].items, 1);
+table.insert(ProceduralDistributions["list"]["FridgeBreakRoom"].items, "ItsAlwaysSunnyInLouisville.FightMilk");
+table.insert(ProceduralDistributions["list"]["FridgeBreakRoom"].items, 1);
+table.insert(ProceduralDistributions["list"]["FridgeOffice"].items, "ItsAlwaysSunnyInLouisville.FightMilk");
+table.insert(ProceduralDistributions["list"]["FridgeOffice"].items, 1);
+table.insert(ProceduralDistributions["list"]["FridgeSoda"].items, "ItsAlwaysSunnyInLouisville.FightMilk");
+table.insert(ProceduralDistributions["list"]["FridgeSoda"].items, 2);
+table.insert(ProceduralDistributions["list"]["FridgeTrailerPark"].items, "ItsAlwaysSunnyInLouisville.FightMilk");
+table.insert(ProceduralDistributions["list"]["FridgeTrailerPark"].items, 2);
+
+table.insert(ProceduralDistributions["list"]["FridgeTrailerPark"].items, "ItsAlwaysSunnyInLouisville.RiotJuice");
+table.insert(ProceduralDistributions["list"]["FridgeTrailerPark"].items, 2);
+table.insert(ProceduralDistributions["list"]["MotelFridge"].items, "ItsAlwaysSunnyInLouisville.RiotJuice");
+table.insert(ProceduralDistributions["list"]["MotelFridge"].items, 1);
+table.insert(ProceduralDistributions["list"]["StoreShelfCombo"].items, "ItsAlwaysSunnyInLouisville.RiotJuice");
+table.insert(ProceduralDistributions["list"]["StoreShelfCombo"].items, 1);
+table.insert(ProceduralDistributions["list"]["StoreShelfDrinks"].items, "ItsAlwaysSunnyInLouisville.RiotJuice");
+table.insert(ProceduralDistributions["list"]["StoreShelfDrinks"].items, 1);
+table.insert(ProceduralDistributions["list"]["BarCounterMisc"].items, "ItsAlwaysSunnyInLouisville.RiotJuice");
+table.insert(ProceduralDistributions["list"]["BarCounterMisc"].items, 1);
+table.insert(ProceduralDistributions["list"]["CafeteriaDrinks"].items, "ItsAlwaysSunnyInLouisville.RiotJuice");
+table.insert(ProceduralDistributions["list"]["CafeteriaDrinks"].items, 1);
+table.insert(ProceduralDistributions["list"]["ClassroomDesk"].items, "ItsAlwaysSunnyInLouisville.RiotJuice");
+table.insert(ProceduralDistributions["list"]["ClassroomDesk"].items, 1);
+
+
+
+table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, "ItsAlwaysSunnyInLouisville.Milksteak");
+table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, 1);
+table.insert(ProceduralDistributions["list"]["FridgeTrailerPark"].items, "ItsAlwaysSunnyInLouisville.Milksteak");
+table.insert(ProceduralDistributions["list"]["FridgeTrailerPark"].items, 2);
+
+
+table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, "ItsAlwaysSunnyInLouisville.RumHam");
+table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, 1);
+table.insert(ProceduralDistributions["list"]["FridgeTrailerPark"].items, "ItsAlwaysSunnyInLouisville.RumHam");
+table.insert(ProceduralDistributions["list"]["FridgeTrailerPark"].items, 2);
+
+
+table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, "ItsAlwaysSunnyInLouisville.MacsMacAndCheeseBox");
+table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, 1);
+table.insert(ProceduralDistributions["list"]["FridgeTrailerPark"].items, "ItsAlwaysSunnyInLouisville.MacsMacAndCheeseBox");
+table.insert(ProceduralDistributions["list"]["FridgeTrailerPark"].items, 2);
+table.insert(ProceduralDistributions["list"]["GasStorageCombo"].items, "ItsAlwaysSunnyInLouisville.MacsMacAndCheeseBox");
+table.insert(ProceduralDistributions["list"]["GasStorageCombo"].items, 1);
+table.insert(ProceduralDistributions["list"]["StoreShelfCombo"].items, "ItsAlwaysSunnyInLouisville.MacsMacAndCheeseBox");
+table.insert(ProceduralDistributions["list"]["StoreShelfCombo"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["GasStorageCombo"].items, "ItsAlwaysSunnyInLouisville.FranksCreamPie");
+table.insert(ProceduralDistributions["list"]["GasStorageCombo"].items, 1);
+table.insert(ProceduralDistributions["list"]["GigamartCandy"].items, "ItsAlwaysSunnyInLouisville.FranksCreamPie");
+table.insert(ProceduralDistributions["list"]["GigamartCandy"].items, 2);
+table.insert(ProceduralDistributions["list"]["BakeryPie"].items, "ItsAlwaysSunnyInLouisville.FranksCreamPie");
+table.insert(ProceduralDistributions["list"]["BakeryPie"].items, 2);
+table.insert(ProceduralDistributions["list"]["BakeryKitchenFridge"].items, "ItsAlwaysSunnyInLouisville.FranksCreamPie");
+table.insert(ProceduralDistributions["list"]["BakeryKitchenFridge"].items, 2);
+table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, "ItsAlwaysSunnyInLouisville.FranksCreamPie");
+table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, 1);
+table.insert(ProceduralDistributions["list"]["FridgeTrailerPark"].items, "ItsAlwaysSunnyInLouisville.FranksCreamPie");
+table.insert(ProceduralDistributions["list"]["FridgeTrailerPark"].items, 2);
+
+
+table.insert(ProceduralDistributions["list"]["BarCounterMisc"].items, "ItsAlwaysSunnyInLouisville.GreenPaintDrink");
+table.insert(ProceduralDistributions["list"]["BarCounterMisc"].items, 1);
+table.insert(ProceduralDistributions["list"]["BarCounterLiquor"].items, "ItsAlwaysSunnyInLouisville.GreenPaintDrink");
+table.insert(ProceduralDistributions["list"]["BarCounterLiquor"].items, 1);
+table.insert(ProceduralDistributions["list"]["StoreShelfMechanics"].items, "ItsAlwaysSunnyInLouisville.GreenPaintDrink");
+table.insert(ProceduralDistributions["list"]["StoreShelfMechanics"].items, 1);
+table.insert(ProceduralDistributions["list"]["GarageTools"].items, "ItsAlwaysSunnyInLouisville.GreenPaintDrink");
+table.insert(ProceduralDistributions["list"]["GarageTools"].items, 1);
